cee7621cc0d714bbbc22849cca23794b25a9e117
Adam PM <adam.pm77@gmail.com>
2024-10-11 18:25:39 +0700

    add footer

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -69,110 +69,61 @@
             @yield('content')
         </div>
 
-        <footer id="footer" class="footer-texts-more-lighten">
-            <div class="container">
-                <div class="row py-4 my-5">
-                    <div class="col-md-6 col-lg-3 mb-5 mb-lg-0">
-                        <h5 class="text-4 text-color-light mb-3">CONTACT INFO</h5>
-                        <ul class="list list-unstyled">
-                            <li class="pb-1 mb-2">
-                                <span class="d-block font-weight-normal line-height-1 text-color-light">ADDRESS</span>
-                                1234 Street Name, City, State, USA
-                            </li>
-                            <li class="pb-1 mb-2">
-                                <span class="d-block font-weight-normal line-height-1 text-color-light">PHONE</span>
-                                <a href="tel:+1234567890">Toll Free (123) 456-7890</a>
-                            </li>
-                            <li class="pb-1 mb-2">
-                                <span class="d-block font-weight-normal line-height-1 text-color-light">EMAIL</span>
-                                <a href="mailto:mail@example.com">mail@example.com</a>
-                            </li>
-                            <li class="pb-1 mb-2">
-                                <span class="d-block font-weight-normal line-height-1 text-color-light">WORKING
-                                    DAYS/HOURS </span>
-                                Mon - Sun / 9:00AM - 8:00PM
-                            </li>
-                        </ul>
-                        <ul class="social-icons social-icons-clean-with-border social-icons-medium">
-                            <li class="social-icons-instagram">
-                                <a href="http://www.instagram.com/" class="no-footer-css" target="_blank"
-                                    title="Instagram"><i class="fab fa-instagram"></i></a>
-                            </li>
-                            <li class="social-icons-twitter mx-2">
-                                <a href="http://www.twitter.com/" class="no-footer-css" target="_blank"
-                                    title="Twitter"><i class="fab fa-twitter"></i></a>
-                            </li>
-                            <li class="social-icons-facebook">
-                                <a href="http://www.facebook.com/" class="no-footer-css" target="_blank"
-                                    title="Facebook"><i class="fab fa-facebook-f"></i></a>
-                            </li>
-                        </ul>
+        <footer id="footer" class="bg-color-primary border-top-0">
+            <div class="container pt-1">
+                <div class="row py-5 text-center text-lg-start">
+                    <div class="col-12 col-lg-4">
+                        <img src="{{ asset('img/sinergi-logo.png') }}" alt="Logo"
+                            class="pe-lg-5 mb-4 mb-lg-0 logo-footer-custom">
+                        <p class="text-pt-footer">PT SINERGI RODIKA UTAMA</p>
+                        <p class="text-pt-footer-small">Electrical And Scada System Energy And Environment Management
+                            Painting
+                            System And Water
+                            Treatment</p>
                     </div>
-                    <div class="col-md-6 col-lg-2 mb-5 mb-lg-0">
-                        <h5 class="text-4 text-color-light mb-3">USEFUL LINKS</h5>
-                        <ul class="list list-unstyled mb-0">
-                            <li class="mb-0"><a href="contact-us-1.html">Help Center</a></li>
-                            <li class="mb-0"><a href="about-us.html">About Us</a></li>
-                            <li class="mb-0"><a href="contact-us.html">Contact Us</a></li>
-                            <li class="mb-0"><a href="page-careers.html">Careers</a></li>
-                            <li class="mb-0"><a href="blog-grid-4-columns.html">Blog</a></li>
-                            <li class="mb-0"><a href="#">Our Location</a></li>
-                            <li class="mb-0"><a href="#">Privacy Policy</a></li>
-                            <li class="mb-0"><a href="sitemap.html">Sitemap</a></li>
-                        </ul>
-                    </div>
-                    <div class="col-md-6 col-lg-4 mb-5 mb-md-0">
-                        <h5 class="text-4 text-color-light mb-3">RECENT NEWS</h5>
-                        <article class="mb-3">
-                            <a href="blog-post.html" class="text-color-light text-3-5">Why should I buy a Web
-                                Template?</a>
-                            <p class="line-height-2 mb-0"><a href="#">Nov 25, 2020</a> in <a
-                                    href="#">Design,</a> <a href="#">Coding</a></p>
-                        </article>
-                        <article class="mb-3">
-                            <a href="blog-post.html" class="text-color-light text-3-5">Creating Amazing Website with
-                                Porto</a>
-                            <p class="line-height-2 mb-0"><a href="#">Nov 25, 2020</a> in <a
-                                    href="#">Design,</a> <a href="#">Coding</a></p>
-                        </article>
-                        <article>
-                            <a href="blog-post.html" class="text-color-light text-3-5">Best Practices for Top UI
-                                Design</a>
-                            <p class="line-height-2 mb-0"><a href="#">Nov 25, 2020</a> in <a
-                                    href="#">Design,</a> <a href="#">Coding</a></p>
-                        </article>
-                    </div>
-                    <div class="col-md-6 col-lg-3">
-                        <h5 class="text-4 text-color-light mb-3">SUBSCRIBE NEWSLETTER</h5>
-                        <p class="mb-2">Get all the latest information on events, sales and offers. Sign up for
-                            newsletter:</p>
-                        <div class="alert alert-success d-none" id="newsletterSuccess">
-                            <strong>Success!</strong> You've been added to our email list.
+                    <div class="col-md-6 col-lg-4">
+                        <div class="feature-box feature-box-style-2 float-lg-end d-block d-lg-flex mb-4 mb-lg-0">
+                            <div class="feature-box-icon">
+                                <i class="icon-location-pin icons text-color-light"></i>
+                            </div>
+                            <div class="feature-box-info ps-1">
+                                <h5 class="mb-0 opacity-7 font-weight-light">ADDRESS</h5>
+                                <p class="text-color-light font-weight-semibold mb-0">MON - FRI: 10:00am - 6:00pm</p>
+                            </div>
                         </div>
-                        <div class="alert alert-danger d-none" id="newsletterError"></div>
-                        <form id="newsletterForm" class="form-style-5 opacity-10"
-                            action="php/newsletter-subscribe.php" method="POST">
-                            <div class="row">
-                                <div class="form-group col">
-                                    <input class="form-control" placeholder="Email Address" name="newsletterEmail"
-                                        id="newsletterEmail" type="text" />
-                                </div>
+                    </div>
+                    <div class="col-md-6 col-lg-4">
+                        <div class="feature-box feature-box-style-2 float-lg-end d-block d-lg-flex mb-4 mb-lg-0">
+                            <div class="feature-box-icon">
+                                <i class="icon-call-out icons text-color-light"></i>
                             </div>
-                            <div class="row">
-                                <div class="form-group col">
-                                    <button class="btn btn-primary btn-rounded btn-px-4 btn-py-2 font-weight-bold"
-                                        type="submit">SUBSCRIBE</button>
-                                </div>
+                            <div class="feature-box-info ps-1">
+                                <h5 class="mb-0 opacity-7 font-weight-light">CALL US NOW</h5>
+                                <a href="tel:+8001234567"
+                                    class="text-color-light font-weight-semibold text-decoration-none">800-123-4567</a>
                             </div>
-                        </form>
+                        </div>
                     </div>
                 </div>
             </div>
-            <div class="container">
-                <div class="footer-copyright footer-copyright-style-2 pt-4 pb-5">
-                    <div class="row">
-                        <div class="col-12 text-center">
-                            <p class="mb-0">Porto Template © 2023. All Rights Reserved</p>
+            <div class="footer-copyright footer-copyright-style-2">
+                <div class="container py-2">
+                    <div class="row py-4">
+                        <div class="col-lg-8 text-center text-lg-start mb-2 mb-lg-0">
+                            <nav id="sub-menu">
+                                <ul>
+                                    <li><i class="fas fa-angle-right"></i><a href="page-faq.html"
+                                            class="ms-1 text-decoration-none"> FAQ's</a></li>
+                                    <li><i class="fas fa-angle-right"></i><a href="sitemap.html"
+                                            class="ms-1 text-decoration-none"> Sitemap</a></li>
+                                    <li><i class="fas fa-angle-right"></i><a href="contact-us.html"
+                                            class="ms-1 text-decoration-none"> Contact Us</a></li>
+                                </ul>
+                            </nav>
+                        </div>
+                        <div
+                            class="col-lg-4 d-flex align-items-center justify-content-center justify-content-lg-end mb-4 mb-lg-0 pt-4 pt-lg-0">
+                            <p>© Copyright 2023. All Rights Reserved.</p>
                         </div>
                     </div>
                 </div>
